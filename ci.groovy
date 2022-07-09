5a9107558a6394849eecff250e6eeb8d450d0800
glend@ <glend@unicourt.com>
2022-07-09 18:02:02 +0530

    Final Changes

--- ci.groovy
+++ ci.groovy
@@ -5,7 +5,7 @@ pipeline {
             steps {
                 sh 'cd $WORKSPACE && rm -rf *'
                 sh 'git clone https://github.com/glendsoza/projCert.git'
-                    sh 'ansible-playbook --inventory /tmp/inv $WORKSPACE/projCerty/deploy-php.yml --extra-vars "env=test build_tag=$BUILD_NUMBER"'
+                    sh 'ansible-playbook --inventory /tmp/inv $WORKSPACE/projCerty/deploy-php.yaml --extra-vars "env=test build_tag=$BUILD_NUMBER"'
                 }
                 post { 
                     always { 
